dbd66c73362a8d1a1ba7449893c8543c80d5e6d6
HaRshA10D <iamharshasmart@gmail.com>
2017-06-03 04:02:59 +0530

    Regression

--- Logistic Regression/logRegres.py
+++ Logistic Regression/logRegres.py
@@ -4,6 +4,7 @@ Created on Thu Jun  1 18:16:31 2017
 
 @author: Harsha SlimShady
 """
+from numpy import *
 
 def loadDataSet():
     dataMat = []; labelMat = []
@@ -29,6 +30,50 @@ def gradAscent(dataMatIn,classLabels):
         error = labelMat - h
         weights = weights + alpha * dataMatrix.transpose()* error   
     return weights
+        
+def plotBestFit(wei):
+    import matplotlib.pyplot as plt
+    weights = array(wei)
+    dataMat,labelMat = loadDataSet()
+    dataArr = mat(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]); ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]); ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0,3.0,0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('x1');plt.ylabel('x2');
+    plt.show()
+    
+    
+def stocGradAscent0(dataMatrix,classLabels):
+    m,n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i]*weights))
+        error = classLabels[i] - h
+        weights = weights + alpha*error*dataMatrix[i]
+    return weights
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
     
